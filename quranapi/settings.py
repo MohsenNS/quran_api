6d837466ec2e3f11d1ea1c701b2112e3b9ad280a
MohsenNS <mohsennoroozs@gmail.com>
2025-04-20 23:32:07 +0330

    liara file correction2

--- quranapi/settings.py
+++ quranapi/settings.py
@@ -79,7 +79,8 @@ WSGI_APPLICATION = 'quranapi.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'database',
     }
 }
 
