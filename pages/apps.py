23768637af27b6b487d7cbdf443301a678ba8089
MohsenNS <mohsennoroozs@gmail.com>
2025-07-31 16:58:10 +0330

    little change2

--- pages/apps.py
+++ pages/apps.py
@@ -8,8 +8,29 @@
 
 #     def ready(self):
 #         threading.Thread(target=eitaa_selenium.start_bot).start()
-import os
+
+
+
+# import os
+# import threading
+# from django.apps import AppConfig
+# from . import eitaa_selenium
+
+# class PagesConfig(AppConfig):
+#     default_auto_field = 'django.db.models.BigAutoField'
+#     name = 'pages'
+
+#     def ready(self):
+#         # if os.environ.get('RUN_MAIN') != 'true':
+#         #     # Prevents execution during the initial runserver fork
+#         #     return
+
+#         print("Running start_bot() only once")
+#         threading.Thread(target=eitaa_selenium.start_bot, daemon=True).start()
+
+
 import threading
+import traceback
 from django.apps import AppConfig
 from . import eitaa_selenium
 
@@ -18,10 +39,14 @@ class PagesConfig(AppConfig):
     name = 'pages'
 
     def ready(self):
-        # if os.environ.get('RUN_MAIN') != 'true':
-        #     # Prevents execution during the initial runserver fork
-        #     return
+        def run_wrapper():
+            try:
+                print("[BOT] start_bot() launching")  # visible in pm2 logs
+                eitaa_selenium.start_bot()
+                print("[BOT] start_bot() returned")
+            except Exception:
+                print("[BOT] exception in start_bot():")
+                traceback.print_exc()
 
-        print("Running start_bot() only once")
-        threading.Thread(target=eitaa_selenium.start_bot, daemon=True).start()
+        threading.Thread(target=run_wrapper, daemon=True).start()
 
