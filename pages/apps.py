484d9585eef52062ea292ddcd01ec9ab82c3f242
MohsenNS <mohsennoroozs@gmail.com>
2025-08-04 00:25:28 +0330

    try something else

--- pages/apps.py
+++ pages/apps.py
@@ -11,26 +11,8 @@
 
 
 
-# import os
-# import threading
-# from django.apps import AppConfig
-# from . import eitaa_selenium
-
-# class PagesConfig(AppConfig):
-#     default_auto_field = 'django.db.models.BigAutoField'
-#     name = 'pages'
-
-#     def ready(self):
-#         # if os.environ.get('RUN_MAIN') != 'true':
-#         #     # Prevents execution during the initial runserver fork
-#         #     return
-
-#         print("Running start_bot() only once")
-#         threading.Thread(target=eitaa_selenium.start_bot, daemon=True).start()
-
-
+import os
 import threading
-import traceback
 from django.apps import AppConfig
 from . import eitaa_selenium
 
@@ -39,14 +21,32 @@ class PagesConfig(AppConfig):
     name = 'pages'
 
     def ready(self):
-        def run_wrapper():
-            try:
-                print("[BOT] start_bot() launching")  # visible in pm2 logs
-                eitaa_selenium.start_bot()
-                print("[BOT] start_bot() returned")
-            except Exception:
-                print("[BOT] exception in start_bot():")
-                traceback.print_exc()
+        # if os.environ.get('RUN_MAIN') != 'true':
+        #     # Prevents execution during the initial runserver fork
+        #     return
 
-        threading.Thread(target=run_wrapper, daemon=True).start()
+        print("Running start_bot() only once")
+        threading.Thread(target=eitaa_selenium.start_bot, daemon=True).start()
+
+
+# import threading
+# import traceback
+# from django.apps import AppConfig
+# from . import eitaa_selenium
+
+# class PagesConfig(AppConfig):
+#     default_auto_field = 'django.db.models.BigAutoField'
+#     name = 'pages'
+
+#     def ready(self):
+#         def run_wrapper():
+#             try:
+#                 print("[BOT] start_bot() launching")  # visible in pm2 logs
+#                 eitaa_selenium.start_bot()
+#                 print("[BOT] start_bot() returned")
+#             except Exception:
+#                 print("[BOT] exception in start_bot():")
+#                 traceback.print_exc()
+
+#         threading.Thread(target=run_wrapper, daemon=True).start()
 
